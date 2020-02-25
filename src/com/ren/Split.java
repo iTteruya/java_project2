d3a63c5708949fd3500c0f86bd85539b4c8f7163
itTeruya <timur.ivanov@outlook.com>
2020-02-25 23:51:44 +0300

    Initial commit

---
+++ src/com/ren/Split.java
@@ -0,0 +1,6 @@
+package com.ren;
+
+public class Split {
+}
+
+
---
+++ test/com/ren/SplitTest.java
@@ -0,0 +1,8 @@
+package com.ren;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SplitTest {
+
+}
